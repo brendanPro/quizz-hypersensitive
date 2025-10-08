eaa0ae8b05d6baf8f01eff04672de15481688fe5
brendanPro <brendan.gouin.pro@gmail.com>
2025-10-08 18:03:10 +0200

    feat: add percent

--- src/components/admin/columns.tsx
+++ src/components/admin/columns.tsx
@@ -2,6 +2,7 @@ import { type ColumnDef } from '@tanstack/react-table';
 import { formatDate } from 'date-fns';
 import ColumnActions from './columnActions';
 import type { Result } from '@/types/result';
+import { MAX_VALUE_PER_QUESTION, QUESTIONS_LABELS } from '@/constants/questions';
 
 export const columns: ColumnDef<Result>[] = [
   {
@@ -19,6 +20,16 @@ export const columns: ColumnDef<Result>[] = [
   {
     accessorKey: 'scoreTotal',
     header: 'Score total',
+  },
+  {
+    id: 'scorePercent',
+    header: 'Score Percent',
+    cell: ({ row }) => {
+      const max = QUESTIONS_LABELS.length * MAX_VALUE_PER_QUESTION;
+      const score = row.original.scoreTotal;
+      const percent = Math.round((score / max) * 100);
+      return <div>{percent}%</div>;
+    },
   },
   {
     id: 'actions',
